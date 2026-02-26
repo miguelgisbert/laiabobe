4d2c8b2c576182ede49bf8e1354ff965f21f86d3
Miguel Gisbert <info@miguelgisbert.dev>
2026-02-26 13:55:11 +0100

    Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastProvider } from "../components/ToastProvider";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,7 +36,10 @@ export default function RootLayout({
   return (
     <html lang="ca">
       <body className={inter.className}>
-        <ToastProvider>{children}</ToastProvider>
+        <ToastProvider>
+          {children}
+        </ToastProvider>
+        <Analytics />
       </body>
     </html>
   );
